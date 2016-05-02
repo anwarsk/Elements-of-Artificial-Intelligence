e2094b5e934fe15996b2dfdf4715b64e1f262d1c
Anwar <mail2anwarshaikh@gmail.com>
2016-05-02 01:33:17 -0400

    Update Launcher for testing purposes

--- Inference Algorithms/Inference Algorithms/src/com/execute/Launcher.java
+++ Inference Algorithms/Inference Algorithms/src/com/execute/Launcher.java
@@ -1,7 +1,15 @@
 package com.execute;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.algorithm.InferenceAlgorithm;
+import com.algorithm.InferenceAlogrithmFactory;
 import com.data.BayesianNetwork;
 import com.data.Constant;
+import com.data.EventNode;
 
 /**
  * This class responsible for launching the program and validating input 
@@ -20,10 +28,32 @@ public class Launcher {
 		
 		String errorMessage = "";
 		String algorithm = args[1];
-		
+		algorithm = "r";
 		if(Constant.alogrithms.containsKey(algorithm))
 		{
 			BayesianNetwork network = new BayesianNetwork();
+			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
+			/**
+			 *  TO-TEST
+			 */
+			Map<EventNode, Boolean> evidence = new HashMap<EventNode, Boolean>();
+			evidence.put(network.getEventNode("E"), true);
+			evidence.put(network.getEventNode("J"), false);
+			List<EventNode> query = new ArrayList<>();
+			query.add(network.getEventNode("M"));
+			query.add(network.getEventNode("A"));
+			long sampleCount = 1000;
+			
+			Map<EventNode, Float> result = inferenceAlgorithm.infer(evidence, query, sampleCount, network);
+			
+			/**
+			 * TO-TEST
+			 */
+			
+			for(EventNode node : result.keySet())
+			{
+				System.out.println("NODE: " + node.getName() + "\t" + result.get(node));
+			}
 		}
 		else
 		{
