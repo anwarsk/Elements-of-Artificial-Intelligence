fd7982b0d872c85fd5b18568e6ab17b610030059
Anwar <mail2anwarshaikh@gmail.com>
2016-05-02 01:32:37 -0400

    Updated probability calculations

--- Inference Algorithms/Inference Algorithms/src/com/data/ProbabilityValue.java
+++ Inference Algorithms/Inference Algorithms/src/com/data/ProbabilityValue.java
@@ -12,15 +12,15 @@ import java.util.List;
 public class ProbabilityValue {
 
 	List<Float> values;
-	
+
 	ProbabilityValue(List<EventNode> parentNodes, List<Float> values)
 	{
 		assert(parentNodes == null || values.size() == Math.pow(2, parentNodes.size())) : "Mismatch number of values and parents (1.1)";
 		assert(parentNodes != null || values.size() == 0) : "Mismatch number of values and parents (1.2)";
-		
+
 		this.values = values;
 	}
-	
+
 	public float getProbability(List<EventNode> parentNodes)
 	{
 		assert(parentNodes == null || values.size() == Math.pow(2, parentNodes.size())) : "Mismatch number of values and parents (2.1)";
@@ -28,15 +28,39 @@ public class ProbabilityValue {
 
 		float probability = 0;
 		int listIndex = 0;
-		
+
 		// Generating list of the index to get the probability value
-		for(EventNode event : parentNodes)
+		if(parentNodes != null)
 		{
-			listIndex = (listIndex << 1) | (event.isValue() ? 1 : 0);
+			for(EventNode event : parentNodes)
+			{
+				listIndex = (listIndex << 1) | (event.isValue() ? 1 : 0);
+			}
 		}
-		
+
 		probability = values.get(listIndex);
-		
+
+		return probability;
+	}
+
+	public float getProbability(boolean...parentValues)
+	{
+		assert(parentValues == null || values.size() == Math.pow(2, parentValues.length)) : "Mismatch number of values and parents (4.1)";
+		assert(parentValues != null || values.size() == 0) : "Mismatch number of values and parents (4.2)";
+
+		float probability = 0;
+		int listIndex = 0;
+
+		// Generating list of the index to get the probability value
+
+		for(boolean parentValue : parentValues)
+		{
+			listIndex = (listIndex << 1) | (parentValue ? 1 : 0);
+		}
+
+
+		probability = values.get(listIndex);
+
 		return probability;
 	}
 }
