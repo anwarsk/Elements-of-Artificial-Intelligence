db5d266866e9a4f783a2e1f23dd8e87cd832d54e
Anwar <mail2anwarshaikh@gmail.com>
2016-05-02 01:28:53 -0400

    Implemented InferenceAlgorithmFactory

--- Inference Algorithms/Inference Algorithms/src/com/algorithm/InferenceAlogrithmFactory.java
+++ Inference Algorithms/Inference Algorithms/src/com/algorithm/InferenceAlogrithmFactory.java
@@ -10,13 +10,33 @@ import com.data.Constant;
  *
  */
 public class InferenceAlogrithmFactory {
-	
+
 	public static InferenceAlgorithm getAlgorithm(String algorithmName)
 	{
 		assert(Constant.alogrithms.containsKey(algorithmName)) : "Invalid algorithm specified.";
-		
+
 		InferenceAlgorithm algorithm = null;
-		
+
+		switch (algorithmName)
+		{
+		case "e":
+			algorithm = new Enumeration();
+			break;
+
+		case "p":
+			algorithm = new PriorSampling();
+			break;
+
+		case "r":
+			algorithm = new RejectionSampling();
+			break;
+
+		case "l":
+			algorithm = new LikelihoodWeighting();
+			break;
+
+		}
+
 		return algorithm;
 	}
 }
