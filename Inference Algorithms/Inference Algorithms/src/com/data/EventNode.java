f24a84c6bd3fcb24ad597c0ae25f98e09cdc2718
Anwar <mail2anwarshaikh@gmail.com>
2016-05-03 05:05:11 -0400

    Updated EventNode class for adding Generic functions

--- Inference Algorithms/Inference Algorithms/src/com/data/EventNode.java
+++ Inference Algorithms/Inference Algorithms/src/com/data/EventNode.java
@@ -18,20 +18,24 @@ public class EventNode {
 	private List<EventNode> childNodes;
 	private boolean value;
 	private ProbabilityValue probabilityValue;
-	
+
 	public EventNode(String name) {
 		this.name = name;
 	}
-	
+
 	public List<EventNode> getParentNodes() {
 		if(parentNodes == null) {
 			parentNodes = new ArrayList<EventNode>();
 		}
 		return parentNodes;
 	}
-	
+
 	public void setParentNodes(EventNode...parentNodes) {
 		Collections.addAll(this.getParentNodes(), parentNodes);
+	}
+
+	public void setParentNodes(List<EventNode>parentNodes) {
+		this.getParentNodes().addAll(parentNodes);
 	}
 	
 	public List<EventNode> getChildNodes() {
@@ -40,15 +44,15 @@ public class EventNode {
 		}
 		return childNodes;
 	}
-	
+
 	public void setChildNodes(EventNode...childNodes) {
 		Collections.addAll(this.getChildNodes(), childNodes);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public boolean isValue() {
 		return value;
 	}
@@ -58,31 +62,52 @@ public class EventNode {
 	}
 
 	public float getProbabilityValue() {
-		
+
 		return probabilityValue.getProbability(this.getParentNodes());
+	}
+
+	public float getProbabilityWithBooleanValue() {
+
+		float value = probabilityValue.getProbability(this.getParentNodes());
+
+		if(!this.value)
+		{
+			value = 1-value;
+		}
+		return value;
 	}
 
 	public void setProbabilityValue(ProbabilityValue probabilityValue) {
 		this.probabilityValue = probabilityValue;
 	}
-	
+
 	public void setProbabilityValue(Float...values) {
-		
+
 		assert(parentNodes == null || values.length == Math.pow(2, parentNodes.size())) : "Mismatch number of values and parents (3.1)";
 		assert(parentNodes != null || values.length == 0) : "Mismatch number of values and parents (3.2)";
-		
+
 		List<Float> valuesList = new ArrayList<Float>();
 		Collections.addAll(valuesList, values);
-		
+
 		this.probabilityValue = new ProbabilityValue(parentNodes, valuesList);
 	}
-	
+
+	public void setProbabilityValue(List<Float> values) {
+
+		assert(parentNodes == null || values.size() == Math.pow(2, parentNodes.size())) : "Mismatch number of values and parents (3.1)";
+		assert(parentNodes != null || values.size() == 0) : "Mismatch number of values and parents (3.2)";
+
+
+		this.probabilityValue = new ProbabilityValue(parentNodes, values);
+	}
+
 	public float getProbabilityValue(boolean...parentValues)
 	{
 		assert(parentNodes == null || parentValues.length == Math.pow(2, parentNodes.size())) : "Mismatch number of values and parents (4.1)";
 		assert(parentNodes != null || parentValues.length == 0) : "Mismatch number of values and parents (4.2)";
-		
+
 		return probabilityValue.getProbability(parentValues);
+
 	}
 
 	public boolean getBooleanValue() {
