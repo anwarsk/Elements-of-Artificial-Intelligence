7a4b23de75e8629769146bd552dc29a1c80c143d
Anwar <mail2anwarshaikh@gmail.com>
2016-05-03 23:13:59 -0400

    Updated OutputWriter

--- Inference Algorithms/Inference Algorithms/src/com/execute/Launcher.java
+++ Inference Algorithms/Inference Algorithms/src/com/execute/Launcher.java
@@ -1,6 +1,9 @@
 package com.execute;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 
 import com.algorithm.InferenceAlgorithm;
 import com.algorithm.InferenceAlogrithmFactory;
@@ -14,7 +17,7 @@ import com.io.OutputWriter;
  * This class responsible for launching the program and validating input 
  * parameters.
  * 
- * @author Anwar
+ * 
  *
  */
 public class Launcher {
@@ -29,63 +32,84 @@ public class Launcher {
 		 * INPUT FORMAT: java my_class r<Algorithm_name> 100<No_of_samples>
 		 */
 		assert(args.length > 3): "Invalid number of input parameters";
-		
+
+
 		String errorMessage = "";
 		String algorithm = args[0];
 		long sampleCount = Long.parseLong(args[1]);
-		
+
 		if(Constant.alogrithms.containsKey(algorithm))
 		{
 			BayesianNetwork network = new BayesianNetwork();
 			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
-			
-			
+
+
 			/**
 			 *  TO-TEST
 			 */
-//			algorithm = "e";
-//		    long sampleCount = 1000;
-//			Map<EventNode, Boolean> evidence = new HashMap<EventNode, Boolean>();
-//			evidence.put(network.getEventNode("J"), true);
-//			evidence.put(network.getEventNode("E"), false);
-////			evidence.put(network.getEventNode("B"), false);
-//			List<EventNode> query = new ArrayList<>();
-//			query.add(network.getEventNode("B"));
-//			query.add(network.getEventNode("M"));
-			
+			//			algorithm = "e";
+			//		    long sampleCount = 1000;
+			//			Map<EventNode, Boolean> evidence = new HashMap<EventNode, Boolean>();
+			//			evidence.put(network.getEventNode("J"), true);
+			//			evidence.put(network.getEventNode("E"), false);
+			////			evidence.put(network.getEventNode("B"), false);
+			//			List<EventNode> query = new ArrayList<>();
+			//			query.add(network.getEventNode("B"));
+			//			query.add(network.getEventNode("M"));
+
 			InputReader inputReader = new InputReader();
 			if(inputReader.readInput(network, Constant.inputFilePath)) {
-				Map<EventNode, Float> result = inferenceAlgorithm.infer(inputReader.getEvidence(),
-															inputReader.getQuery(), sampleCount, network);
-				
+				ArrayListValuedHashMap<EventNode, Float> result = new ArrayListValuedHashMap<EventNode, Float>();
+
+				for(int iterationIndex =0; iterationIndex <10; iterationIndex++)
+				{
+					result.putAll(inferenceAlgorithm.infer(inputReader.getEvidence(),
+							inputReader.getQuery(), sampleCount, network));
+
+				}
+
 				OutputWriter outputWriter = new OutputWriter();
-				outputWriter.writeOutput(result, Constant.outputFilePath);
+
+				Map<EventNode, Float> collatedResult = new HashMap<EventNode, Float>();
+
+				for(EventNode node : inputReader.getQuery())
+				{
+					float collatedValue = 0;
+					for(float value : result.get(node))
+					{
+						collatedValue += value;
+					}
+					collatedValue = collatedValue / result.get(node).size();
+					collatedResult.put(node, collatedValue);
+				}
+
+				outputWriter.writeOutput(collatedResult, Constant.outputFilePath);
 			}
-			
+
 			/**
 			 * TO-TEST
 			 */
-//			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
-//			Map<EventNode, Float> result = inferenceAlgorithm.infer(evidence, query, sampleCount, network);
-//			
-//			System.out.println("******* SAMPLE SIZE : " + sample +" *******");
-//			for(EventNode node : result.keySet())
-//			{
-//				System.out.println("NODE: " + node.getName() + "\t" + result.get(node));
-//			}
-//			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
-//			Map<EventNode, Float> result = inferenceAlgorithm.infer(evidence, query, sampleCount, network);
-//			
-//				for(EventNode node : result.keySet())
-//			{
-//				System.out.println("NODE: " + node.getName() + "\t" + result.get(node));
-//			}
+			//			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
+			//			Map<EventNode, Float> result = inferenceAlgorithm.infer(evidence, query, sampleCount, network);
+			//			
+			//			System.out.println("******* SAMPLE SIZE : " + sample +" *******");
+			//			for(EventNode node : result.keySet())
+			//			{
+			//				System.out.println("NODE: " + node.getName() + "\t" + result.get(node));
+			//			}
+			//			InferenceAlgorithm inferenceAlgorithm  = InferenceAlogrithmFactory.getAlgorithm(algorithm);
+			//			Map<EventNode, Float> result = inferenceAlgorithm.infer(evidence, query, sampleCount, network);
+			//			
+			//				for(EventNode node : result.keySet())
+			//			{
+			//				System.out.println("NODE: " + node.getName() + "\t" + result.get(node));
+			//			}
 		}
 		else
 		{
 			errorMessage = "Invalid algorithm specified.";
 		}
-		
+
 		System.out.println(errorMessage);
 	}
 
--- Inference Algorithms/Inference Algorithms/src/com/io/OutputWriter.java
+++ Inference Algorithms/Inference Algorithms/src/com/io/OutputWriter.java
@@ -3,10 +3,6 @@
  */
 package com.io;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Map;
 
 import com.data.EventNode;
@@ -17,7 +13,7 @@ import com.data.EventNode;
  * @author Anwar
  */
 public class OutputWriter {
-	
+
 	/**
 	 * Write output.
 	 *
@@ -28,34 +24,15 @@ public class OutputWriter {
 	public boolean writeOutput(Map<EventNode, Float> resultProbabilities, String outputFilePath)
 	{
 		boolean isSuccessful = false;
-		BufferedWriter bw = null;
-		try {
-			File file = new File(outputFilePath);
 
-			// if file doesn't exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-
-			bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
-			for(EventNode node : resultProbabilities.keySet()) {
-				bw.write(node.getName() + " " + resultProbabilities.get(node) + "\n");
-				System.out.println(node.getName() + " " + resultProbabilities.get(node));
-			}
-			
-			// successful = true
-			isSuccessful = true;
-		} catch (IOException ex) {
-			System.err.println("Exception writing output to file: " + ex);
-		} finally {
-			if(bw != null) {
-				try {
-					bw.close();
-				} catch (IOException ex) {
-					System.err.println("Exception closing output stream: " + ex);
-				}
-			}
+		for(EventNode node : resultProbabilities.keySet()) 
+		{
+			System.out.print(node.getName() + " " + resultProbabilities.get(node));
 		}
+
+		// successful = true
+		isSuccessful = true;
+
 		return isSuccessful;
 	}
 
